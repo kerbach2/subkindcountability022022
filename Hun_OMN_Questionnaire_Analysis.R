bdcfe45e78bcd531bfff158b24694302ae56384c
kerbach2 <52123390+kerbach2@users.noreply.github.com>
2022-04-20 15:33:27 +0200

    Add files via upload

--- Hun_OMN_Questionnaire_Analysis.R
+++ Hun_OMN_Questionnaire_Analysis.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(effsize)
 library(lmerTest)
+library(multcomp)
 library(emmeans)
 
 # import data
@@ -10,30 +11,42 @@ data <- read_csv('Hun_OMN_Questionnaire_Data.csv')
 
 # create subsets for analayses
 ## object denoting nouns
+
+
 objects <- filter(data, class %in% c('object'))
 ## mass nouns ~ subkinds 
-masssk <- filter(data, `counted-entity` %in% c('subkind'))
-masssk <- filter(masssk, `object-countability` %in% c('uncountable'))
+masssk <- filter(data, `counted-entities` %in% c('subkind'))
+masssk <- filter(masssk, `object-countability` %in% c('mass'))
 ## object mass nouns counting subkinds
-om <- filter(objects, `object-countability` %in% c('uncountable'))
-omsk <- filter(om, Condition %in% c('subkind'))
+om <- filter(objects, `object-countability` %in% c('mass'))
+omsk <- filter(om, `counted-entities` %in% c('subkind'))
 ## substance mass nouns counting subkinds
-smsk <- filter(masssk, Class %in% c('substance'))
+smsk <- filter(masssk, class %in% c('substance'))
 
 #Draw graphs
-masssk %>% ggplot(aes(x = class, y = rating, fill = Class)) + geom_boxplot() + theme_minimal() + scale_fill_brewer(palette = 'PuOr')
+masssk %>% ggplot(aes(x = class, y = rating, fill = class)) + geom_boxplot() + theme_minimal() + scale_fill_brewer(palette = 'Greys')
 
-objects %>% ggplot(aes(y = rating, x = `object-countability`, fill = Condition)) + geom_boxplot() + labs(x = "Countability", y = "Judgment") + scale_fill_brewer(palette = 'PuOr')
+objects %>% ggplot(aes(y = rating, x = `object-countability`, fill = `counted-entities`)) + geom_boxplot() + labs(x = "Countability", y = "Judgment") + scale_fill_brewer(palette = 'Greys')
 
 # Unpaired t-test of substance and object mass nouns counting subkinds
-t.test(smsk$Judgment,omsk$Judgment,paired=FALSE)
+t.test(smsk$rating,omsk$rating,paired=FALSE)
 cohen.d(rating ~ class, data = masssk)
 
 # ANOVA
 # To see if there is an interaction of of Countability and Condition
 
-M<-lmer(rating ~ (`object-countability` * `counted-entities`) + (1|subject) + (1|pair), data=objects)
+M<-lmer(rating ~ (`object-countability` * `counted-entities`) + (1|subject) + (0 + `object-countability` * `counted-entities`|subject) + (1|`object-countability` * `counted-entities`|pair), data=objects)
+summary(M)
 anova(M)
 
-# Paired t-Test
-emmeans(M, list(pairwise ~ (`object-countability` * `counted-entities`)))
+# Clean data; remove problematic elements and corresponding pairs
+
+clean <-data[!(data$pair=="bullet-ammunition" | data$pair=="container-plasticware" | data$pair=="good-merchandise" | data$pair=="blanket-bedding" | data$pair=="dish-china"),]
+
+# ANOVA on clean data
+# To see if there is an interaction of of Countability and Condition
+
+Mc<-lmer(rating ~ (`object-countability` * `counted-entities`) + (1|subject) + (0 + `object-countability` * `counted-entities`|subject) + (1|`object-countability` * `counted-entities`|pair), data=clean)
+summary(Mc)
+anova(Mc)
+
